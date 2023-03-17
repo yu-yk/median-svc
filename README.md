6f72c945c9fe7ff683570695a7782682b6a0cdd3
yu-yk <yukkuen.yu@linktivity.co.jp>
2023-03-17 20:56:11 +0900

    grpc-gateway WIP

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # median-svc
 A hands-on practice for GRPC service to calculate median
+
+- [ ]  Use fx
+- [ ]  Use logger
+- [x]  Use grpc
+- [ ]  Use grpc-gateway to generate and export http interface (WIP)
+- [ ]  Familiar with options pattern
+- [ ]  Deploy a Kubernetes application in Minikube
--- main.go
+++ main.go
@@ -1,27 +1,51 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/yu-yk/median-svc/median"
 	"github.com/yu-yk/median-svc/pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
 )
 
 func main() {
+	// gprc
 	listener, err := net.Listen("tcp", ":3000")
 	if err != nil {
 		panic(err)
 	}
+	defer listener.Close()
 
 	grpcServer := grpc.NewServer()
 	medianServer := median.NewServer()
 
 	pb.RegisterMedianServer(grpcServer, medianServer)
 	reflection.Register(grpcServer)
-	if err := grpcServer.Serve(listener); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+
+	go func() {
+		if err := grpcServer.Serve(listener); err != nil {
+			log.Panicf("failed to serve: %v", err)
+		}
+	}()
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	if err := pb.RegisterMedianHandlerFromEndpoint(ctx, mux, ":3000", opts); err != nil {
+		log.Panicf("failed to serve: %v", err)
+	}
+
+	// http
+	if err := http.ListenAndServe(":3001", mux); err != nil {
+		log.Panicf("failed to serve: %v", err)
 	}
 }
